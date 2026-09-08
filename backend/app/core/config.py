52d8b72545301a880dd95fdabfdb33d42152e5a3
Naveen281991 <naveen.j.2805@gmail.com>
2026-09-08 16:43:33 +0530

    fix production FASTAPI environment setting

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -24,8 +24,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost:5173"
-    FASTAPI_ENV: Literal["development"] | None = None
-
+    FASTAPI_ENV: Literal["development", "production"] = "development"
     PROJECT_NAME: str
     SENTRY_DSN: HttpUrl | None = None
     DATABASE_URL: PostgresDsn
